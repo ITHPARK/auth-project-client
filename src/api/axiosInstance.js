67c7517a3edc2c7c7fb85c53724195d521f22827
DESKTOP-4ALDHGC\박태현 <ithpark1035@gmail.com>
2024-07-27 13:33:48 +0900

    주석 추가

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -15,18 +15,21 @@ let isRefreshing = false;
 //토큰 갱신이 완료되었을 때 실행될 콜백 함수들을 저장하는 배열.
 let refreshSubscribers = [];
 
-function subscribeTokenRefresh(cb) {
-  refreshSubscribers.push(cb);
+//item이라는 콜백을 배열에 추가한다.
+function subscribeTokenRefresh(item) {
+  refreshSubscribers.push(item);
 }
 
+//배열 각 콜백함수에 인자를 전달한다.
 function onRefreshed(token) {
-  refreshSubscribers.map((cb) => cb(token));
+  refreshSubscribers.map((item) => item(token));
 }
 
 //특정 쿠키를 가져오는 함수 
 function getCookie(name) {
   return cookies.get(name);
 }
+
 /*
   HTTP 요청이나 응답을 가로채서 처리할 수 있는 기능을 제공한다.
   인터셉터를 사용하면 요청이 서버로 전달되기 전에 혹은 응답이 애플리케이션에 전달되기 전에 특정 로직을 실행한다
@@ -86,8 +89,12 @@ axiosInstance.interceptors.response.use(
           // 쿠키에 새로운 액세스 토큰 저장
           cookies.set('accessToken', newAccessToken, { path: '/' });
 
+          //토큰 갱신 완료
           isRefreshing = false;
+
+          //배열에 
           onRefreshed(newAccessToken);
+
           refreshSubscribers = [];
         } catch (err) {
           isRefreshing = false;
@@ -95,16 +102,21 @@ axiosInstance.interceptors.response.use(
         }
       }
 
+      //promise 객체 토큰이 다시 생성될때까지 기다린다.
       const retryOriginalRequest = new Promise((resolve) => {
+        //콜백 함수를 등록
         subscribeTokenRefresh((token) => {
+
+          //헤더에 토큰을 추가함          
           originalRequest.headers.Authorization = `Bearer ${token}`;
+
+          //기존의 요청을 다시 보낸다.
           resolve(axiosInstance(originalRequest));
+
         });
       });
-
       return retryOriginalRequest;
     }
-
     return Promise.reject(error);
   }
 );
